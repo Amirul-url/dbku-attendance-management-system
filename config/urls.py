b287ef98cfbf5401bb33b9b9ca0d2590e856611f
Amirul-url <muhdamirulaqmal@gmail.com>
2026-04-20 08:26:49 +0800

    change home page to login

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from employees.views import register_page
+from employees.views import login_page
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', register_page),
+    path('', login_page),
     path('api/employees/', include('employees.urls')),
     path('', include('employees.urls')),
 ]
